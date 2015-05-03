55c034987c41b20554607149a8d0a3cb0d8892e7
gordonmj <neologue@gmail.com>
2015-05-03 13:23:42 -0400

    Prohibiting duplicate number seems to be working.

--- Sudoku/Sudoku/Board.cs
+++ Sudoku/Sudoku/Board.cs
@@ -133,8 +133,8 @@ namespace Sudoku
 
         public bool isValidinSquare(int num, int row, int col)
         {
-            row = row % 3;
-            col = col % 3;
+            row = row / 3;
+            col = col / 3;
             for (int r = 0; r < 3; r++)
             {
                 for (int c = 0; c < 3; c++)
